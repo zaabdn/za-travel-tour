70155c5be43814a86fbb4f241d16f995b25cd410
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-09-20 15:17:32 +0700

    api login and register using next auth

---
+++ src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,46 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "../../../../prisma/client";
+import { compare } from "bcryptjs";
+import NextAuth from "next-auth/next";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Invalid Credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error("No user found");
+        }
+
+        const isValid = await compare(credentials.password, user.password);
+
+        if (!isValid) {
+          throw new Error("Incorrect Password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: "",
+};
+
+export default NextAuth(authOptions);
