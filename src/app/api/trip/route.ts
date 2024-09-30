d632dba6467928dc6d8131c38b9f5b46aa53d6d2
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-09-30 14:47:37 +0700

    api trip

---
+++ src/app/api/trip/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../../prisma/client";
+
+export async function GET() {
+  const users = await prisma.trip.findMany();
+
+  return NextResponse.json(
+    {
+      success: true,
+      message: "List trips",
+      data: users,
+    },
+    {
+      status: 200,
+    }
+  );
+}
+
+export async function POST(req: Request) {
+  const { fullName, email, password } = await req.json();
+
+  const trip = await prisma.trip.create({
+    data: {
+      fullName,
+      email,
+      password,
+    },
+  });
+
+  return NextResponse.json(
+    {
+      success: true,
+      message: "Trip created successfully",
+      data: trip,
+    },
+    {
+      status: 201,
+    }
+  );
+}
