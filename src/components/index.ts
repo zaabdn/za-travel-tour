bc8ea86ae22dbbb6c6a90b05d969de77f414d7f4
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-10-02 17:06:10 +0700

    rename file to kebab case

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Header } from "./header/header";
 export { default as Hero } from "./hero/hero";
-export { default as HeaderText } from "./text/headerText";
+export { default as HeaderText } from "./text/header-text";
 export { default as Card } from "./card/card";
 export { default as Footer } from "./footer/footer";
