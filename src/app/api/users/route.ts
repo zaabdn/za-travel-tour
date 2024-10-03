ff229e40d7fcd089513af9b3cef8c3c3091fca95
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-10-03 08:34:23 +0700

    fix api create user (add validation & hash password)

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma/client";
+import { hash } from "bcryptjs";
 
 export async function GET() {
   const users = await prisma.user.findMany();
@@ -17,24 +18,87 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
-  const { fullName, email, password } = await req.json();
+  try {
+    const { fullName, email, password, role } = await req.json();
 
-  const user = await prisma.user.create({
-    data: {
-      fullName,
-      email,
-      password,
-    },
-  });
-
-  return NextResponse.json(
-    {
-      success: true,
-      message: "User created successfully",
-      data: user,
-    },
-    {
-      status: 201,
+    if (!fullName || !email || !password || !role) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Field is required!",
+          data: null,
+        },
+        {
+          status: 40,
+        }
+      );
     }
-  );
+
+    const hashPassword = hash(password, 10);
+
+    const userExist = await prisma.user.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (userExist) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Email is exist",
+          data: null,
+        },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        fullName,
+        email,
+        password: hashPassword,
+        role,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "User created successfully",
+        data: user,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Failed to create user",
+          error: error.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    // Fallback in case the error is not an instance of Error
+    return NextResponse.json(
+      {
+        success: false,
+        message: "An unknown error occurred",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
