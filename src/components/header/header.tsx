af8308cb4710fc44487cda8e7a639e317fefa3ab
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-10-05 18:46:05 +0700

    add sign out

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import Link from "next/link";
-import React from "react";
-import { Button } from "../ui/button";
-import { useSession } from "next-auth/react";
-import { Avatar } from "../ui/avatar";
-import { usePathname } from "next/navigation";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 const Header: React.FC = () => {
-  const { data: user } = useSession();
+  const { data: user, status } = useSession();
+
   const pathname = usePathname();
 
   return (
@@ -32,29 +34,47 @@ const Header: React.FC = () => {
             <Link href={"/"} className="px-5">
               <p className="text-gray-500 hover:text-rose-500">Packet</p>
             </Link> */}
-            {user?.user?.email ? (
-              <Link href={"/"} className="px-5 ">
-                <Avatar />
-              </Link>
+            {user?.user?.email && status == "authenticated" ? (
+              <Popover>
+                <PopoverTrigger asChild className="cursor-pointer">
+                  <Avatar>
+                    <AvatarFallback>
+                      {user.user.email.slice(0, 2).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                </PopoverTrigger>
+                <PopoverContent className="w-50">
+                  <div className="grid gap-4">
+                    <h4
+                      className="font-medium leading-none cursor-pointer"
+                      onClick={() => signOut()}
+                    >
+                      Logout
+                    </h4>
+                  </div>
+                </PopoverContent>
+              </Popover>
             ) : (
-              <div className="flex flex-row">
-                <Link href="/login" className="mr-2">
-                  <Button
-                    variant="outline"
-                    className="py-5 px-8 text-white hover:bg-[#e69e02] hover:text-white hover:border-[#FFAF00]"
-                  >
-                    Login
-                  </Button>
-                </Link>
-                <Link href="/register">
-                  <Button
-                    type="submit"
-                    className="py-5 px-6 border-2 border-[#FFAF00] bg-[#FFAF00] hover:bg-[#e69e02]"
-                  >
-                    Register
-                  </Button>
-                </Link>
-              </div>
+              status !== "loading" && (
+                <div className="flex flex-row">
+                  <Link href="/login" className="mr-2">
+                    <Button
+                      variant="outline"
+                      className="py-5 px-8 text-white hover:bg-[#e69e02] hover:text-white hover:border-[#FFAF00]"
+                    >
+                      Login
+                    </Button>
+                  </Link>
+                  <Link href="/register">
+                    <Button
+                      type="submit"
+                      className="py-5 px-6 border-2 border-[#FFAF00] bg-[#FFAF00] hover:bg-[#e69e02]"
+                    >
+                      Register
+                    </Button>
+                  </Link>
+                </div>
+              )
             )}
           </div>
         )}
