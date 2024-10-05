9231b14a52f81e1637d3cf5680dbdaeb81bf2c8c
AzureAD\ZainalAbidin <zainal609@gmail.com>
2024-10-05 15:02:21 +0700

    fix api create user, install type bcrypt

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,52 +1,72 @@
-import { hash } from "bcryptjs";
+import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
 import prisma from "../../../../../prisma/client";
 
-interface RequestRegister extends Request {
-  email: string;
-  fullName: string;
-  password: string;
-}
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
 
-export async function POST(req: RequestRegister) {
-  const { email, fullName, password } = req;
+    const { email, fullName, password, role } = body;
 
-  if (!email || !fullName || !password) {
-    return NextResponse.json(
-      { success: false, message: "Missing Field" },
-      { status: 400 }
-    );
-  }
-
-  const existingUser = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (existingUser) {
-    return NextResponse.json(
-      { success: false, message: "User already exist" },
-      { status: 400 }
-    );
-  }
-
-  const hashPassword = hash(password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      fullName,
-      password: hashPassword,
-    },
-  });
-
-  return NextResponse.json(
-    {
-      success: true,
-      message: "User created successfully",
-      data: user,
-    },
-    {
-      status: 201,
+    // Check for missing fields
+    if (!email || !fullName || !password || !role) {
+      console.log("Missing fields:", { email, fullName, password, role });
+      return NextResponse.json(
+        { success: false, message: "Missing Field" },
+        { status: 400 }
+      );
     }
-  );
+
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      console.log("User already exists with email:", email);
+      return NextResponse.json(
+        { success: false, message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    // Hash the password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // Create the new user
+    const user = await prisma.user.create({
+      data: {
+        email,
+        fullName,
+        password: hashPassword,
+        role,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "User created successfully",
+        data: user.email,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { success: false, message: "Server Error", error: error.message },
+        { status: 500 }
+      );
+    } else {
+      // Handle unexpected error types
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Server Error",
+          error: "Unknown error occurred",
+        },
+        { status: 500 }
+      );
+    }
+  }
 }
